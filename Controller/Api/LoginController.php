62dff0281b35c9a46860b507808c6c2c0ed56f56
Juan Manuel Aguero <juanma.aguero@gmail>
2015-11-11 22:32:19 -0300

    Login para api

---
+++ Controller/Api/LoginController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Flower\UserBundle\Controller\Api;
+
+use FOS\RestBundle\Controller\FOSRestController;
+use FOS\RestBundle\Util\Codes;
+use FOS\RestBundle\View\View as FOSView;
+use Symfony\Component\HttpFoundation\Request;
+use Flower\UserBundle\Model\User;
+
+class LoginController extends FOSRestController
+{
+    public function loginCheck()
+    {
+        die('mmm');
+    }
+}
