d35890cd3f5452dfc95a25aefa5bc49d747c096b
Juan Manuel Aguero <juanma.aguero@gmail>
2016-01-23 20:44:28 -0300

    Add orgposition model and doctrine extensions as dependencies

---
+++ Model/OrgPosition.php
@@ -0,0 +1,258 @@
+<?php
+
+namespace Flower\UserBundle\Model;
+
+use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Mapping\Annotation as Gedmo;
+
+
+abstract class OrgPosition
+{
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255, nullable=true)
+     */
+    protected $name;
+
+    /**
+     * @var integer
+     *
+     * @Gedmo\TreeLeft
+     * @ORM\Column(name="lft", type="integer")
+     */
+    protected $lft;
+
+    /**
+     * @var integer
+     *
+     * @Gedmo\TreeRight
+     * @ORM\Column(name="rgt", type="integer")
+     */
+    protected $rgt;
+    /**
+     * @var integer
+     *
+     * @Gedmo\TreeLevel
+     * @ORM\Column(name="lvl", type="integer")
+     */
+    protected $lvl;
+    /**
+     * @Gedmo\TreeRoot
+     * @ORM\Column(name="root", type="integer", nullable=true)
+     */
+    protected $root;
+
+    /**
+     * @Gedmo\TreeParent
+     * @ORM\ManyToOne(targetEntity="\Flower\ModelBundle\Entity\OrgPosition", inversedBy="children")
+     * @ORM\JoinColumn(name="parent_id", referencedColumnName="id", onDelete="CASCADE")
+     */
+    protected $parent;
+
+    /**
+     * @ORM\OneToMany(targetEntity="\Flower\ModelBundle\Entity\OrgPosition", mappedBy="parent")
+     * @ORM\OrderBy({"lft" = "ASC"})
+     */
+    protected $children;
+
+    /**
+     * OrgPosition constructor.
+     */
+    public function __construct()
+    {
+        $this->children = new \Doctrine\Common\Collections\ArrayCollection();
+    }
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return \Flower\ModelBundle\Entity\OrgPosition
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set lft
+     *
+     * @param integer $lft
+     * @return \Flower\ModelBundle\Entity\OrgPosition
+     */
+    public function setLft($lft)
+    {
+        $this->lft = $lft;
+        return $this;
+    }
+
+    /**
+     * Get lft
+     *
+     * @return integer
+     */
+    public function getLft()
+    {
+        return $this->lft;
+    }
+
+    /**
+     * Set rgt
+     *
+     * @param integer $rgt
+     * @return \Flower\ModelBundle\Entity\OrgPosition
+     */
+    public function setRgt($rgt)
+    {
+        $this->rgt = $rgt;
+        return $this;
+    }
+
+    /**
+     * Get rgt
+     *
+     * @return integer
+     */
+    public function getRgt()
+    {
+        return $this->rgt;
+    }
+
+    /**
+     * Set lvl
+     *
+     * @param integer $lvl
+     * @return \Flower\ModelBundle\Entity\OrgPosition
+     */
+    public function setLvl($lvl)
+    {
+        $this->lvl = $lvl;
+        return $this;
+    }
+
+    /**
+     * Get lvl
+     *
+     * @return integer
+     */
+    public function getLvl()
+    {
+        return $this->lvl;
+    }
+
+
+    /**
+     * Set root
+     *
+     * @param integer $root
+     * @return \Flower\ModelBundle\Entity\OrgPosition
+     */
+    public function setRoot($root)
+    {
+        $this->root = $root;
+        return $this;
+    }
+
+    /**
+     * Get root
+     *
+     * @return integer
+     */
+    public function getRoot()
+    {
+        return $this->root;
+    }
+
+    /**
+     * Set parent
+     *
+     * @param \Flower\ModelBundle\Entity\OrgPosition $parent
+     * @return \Flower\ModelBundle\Entity\OrgPosition
+     */
+    public function setParent(\Flower\ModelBundle\Entity\OrgPosition $parent = null)
+    {
+        $this->parent = $parent;
+        return $this;
+    }
+
+    /**
+     * Get parent
+     *
+     * @return \Flower\ModelBundle\Entity\OrgPosition
+     */
+    public function getParent()
+    {
+        return $this->parent;
+    }
+
+    /**
+     * Add children
+     *
+     * @param \Flower\ModelBundle\Entity\OrgPosition $children
+     * @return \Flower\ModelBundle\Entity\OrgPosition
+     */
+    public function addChild(\Flower\ModelBundle\Entity\OrgPosition $children)
+    {
+        $this->children[] = $children;
+        return $this;
+    }
+
+    /**
+     * Remove children
+     *
+     * @param \Flower\ModelBundle\Entity\OrgPosition $children
+     */
+    public function removeChild(\Flower\ModelBundle\Entity\OrgPosition $children)
+    {
+        $this->children->removeElement($children);
+    }
+
+    /**
+     * Get children
+     *
+     * @return \Doctrine\Common\Collections\Collection
+     */
+    public function getChildren()
+    {
+        return $this->children;
+    }
+
+    public function __toString()
+    {
+        return $this->name;
+    }
+
+}
